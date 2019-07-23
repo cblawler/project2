c2818ff94349d818aa9d4ca10d9bde99fb8665bc
cblawler <chris.b.lawler@gmail.com>
2019-07-23 14:41:21 -0500

    Added JawsDB to Heroku

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,34 @@ require("dotenv").config();
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
+var config = {
+    port: 3306,
+    host: process.env.HOST_DB,
     user: process.env.USER_DB,
     password: process.env.PASS_DB,
     database: process.env.DB_NAME
-});
+};
+var connection;
+var host;
+
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+    host = 'JAWSDB';
+} else {
+    connection = mysql.createConnection(config);
+    host = process.env.HOST_DB;
+};
+
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: process.env.USER_DB,
+//     password: process.env.PASS_DB,
+//     database: process.env.DB_NAME
+// });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log('connected on MYSQL');
 });
+
+module.exports = connection;
