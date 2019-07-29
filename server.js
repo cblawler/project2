40681e2c31795f1f37b9f81de30c21c714556fd6
Karen <kvicknair@yahoo.com>
2019-07-29 18:05:11 -0500

    make node server work

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-
-//Dependencies 
 var express = require("express");
-// var exphbs = require("express-handlebars");
-var routes = require("./controllers/apiRoutes.js");
+
 var app = express();
-
-
 
 //PORT
 var PORT = process.env.PORT || 8081;
 
-
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
-
-// connection.end();
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -25,15 +13,12 @@ app.use(express.json());
 //Public 
 app.use(express.static("public"));
 
-//Handlebars
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
 
 //Routes
-var routes = ("./controllers/apiRoutes.js");
+var routes = require("./controllers/apiRoutes.js");
 app.use(routes);
 //Listened app
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
-    console.log("Server listening");
+    console.log("Server listening on http://localhost:"+PORT);
 });
